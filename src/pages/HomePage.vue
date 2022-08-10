4a24715ec431bf86478b9e35ce4aa6f47dd0e295
Austin Dye <FireAtWill012496>
2022-08-10 17:15:09 -0600

    layout

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,38 +1,87 @@
 <template>
-  <div class="container-fluid">
-    <div class="row">
-      <div class="col-md-6 bg-primary blue d-flex p-0">
-        <div class="angle-other-3 position-absolute"></div>
-        <div class="angle-other-2 position-absolute"></div>
-        <div class="angle-other-4 position-absolute"></div>
-        <div class="position-absolute word-wrapper">
-          <h1>Hello World</h1>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta ipsa
-            tempore repellat repudiandae ut aperiam atque molestias fuga dolorem
-            dolor dolorum odit, totam doloribus nam delectus! Deleniti molestias
-            soluta aliquid.
-          </p>
-        </div>
-      </div>
-      <div class="col-md-6 d-flex p-0">
-        <div class="angle3 position-absolute"></div>
-        <div class="angle2 position-absolute"></div>
-        <div class="angle4 position-absolute"></div>
-        <div class="angle"></div>
-        <img
-          class="head-img position-absolute"
-          src="src/assets/img/stock-photo-remove-bg.png"
-          alt=""
-        />
-      </div>
-
-      <div
-        class="col-12 text-center bg-secondary second position-absolute second-section"
-      >
-        <h1>loremlorem</h1>
+  <div class="row">
+    <div class="col-12 sticky-top bg-secondary w-100"><h1>Header</h1></div>
+    <div class="col-md-6 bg-primary blue d-flex p-0">
+      <div class="angle-other-3 position-absolute"></div>
+      <div class="angle-other-2 position-absolute"></div>
+      <div class="angle-other-4 position-absolute"></div>
+      <div class="position-absolute word-wrapper">
+        <h1>Hello World</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta ipsa
+          tempore repellat repudiandae ut aperiam atque molestias fuga dolorem
+          dolor dolorum odit, totam doloribus nam delectus! Deleniti molestias
+          soluta aliquid.
+        </p>
       </div>
     </div>
+    <div class="col-md-6 d-flex p-0">
+      <div class="angle3 position-absolute"></div>
+      <div class="angle2 position-absolute"></div>
+      <div class="angle4 position-absolute"></div>
+      <div class="angle"></div>
+      <img
+        class="head-img position-absolute"
+        src="src/assets/img/stock-photo-remove-bg.png"
+        alt=""
+      />
+    </div>
+
+    <div class="col-md-6 d-flex p-0">
+      <div class="angle-other-3 position-absolute"></div>
+      <div class="angle-other-2 position-absolute"></div>
+      <div class="angle-other-4 position-absolute"></div>
+      <div class="angle-other"></div>
+      <img
+        class="head-img position-absolute"
+        src="src/assets/img/stock-photo-remove-bg.png"
+        alt=""
+      />
+    </div>
+    <div class="col-md-6 bg-primary blue d-flex p-0">
+      <div class="">
+        <h1>Hello World</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta ipsa
+          tempore repellat repudiandae ut aperiam atque molestias fuga dolorem
+          dolor dolorum odit, totam doloribus nam delectus! Deleniti molestias
+          soluta aliquid.
+        </p>
+      </div>
+    </div>
+    <div class="col-md-6 p-5">
+      <div class="card p-4">
+        <h1>contract to close</h1>
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Saepe,
+          facilis totam nisi doloribus obcaecati ipsa! Magni repellat dolores
+          sequi vel ipsa neque exercitationem numquam dolorem, enim saepe
+          reiciendis minus assumenda?
+        </p>
+      </div>
+    </div>
+    <div class="col-md-6 p-5">
+      <div class="card p-4">
+        <h1>contract to close</h1>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sapiente
+          odit cumque, exercitationem nobis placeat molestiae veniam, nostrum
+          fugit facere enim reprehenderit quibusdam magnam earum sed? Minima
+          excepturi suscipit itaque aut?
+        </p>
+      </div>
+    </div>
+    <div class="col-12 d-flex justify-content-center p-5">
+      <div class="d-block p-2">
+        <h1>Pricing</h1>
+        <ul class="">
+          <li>lorem lorem lorem : $250</li>
+          <li></li>
+          Ipsum Ipsum Ipsum : $250
+        </ul>
+      </div>
+    </div>
+    <div class="col-12 bg-primary w-100 h-25"><h1>Footer</h1></div>
   </div>
 </template>
 
@@ -91,18 +140,6 @@ export default {
   z-index: 3;
   border-top: 85vh solid rgba($primary, 0.2);
   border-right: 80vw solid transparent;
-}
-
-.angle-other-2,
-.angle-other-3,
-.angle-other-4 {
-}
-
-.angle-other-2 {
-}
-.angle-other-3 {
-}
-.angle-other-4 {
 }
 
 .angle2 {
